fe7a2e608f7cd3b6f213afef2c3023fe192cef5d
Leônidas Araújo <leonidasaraujo19@gmail.com>
2025-01-14 15:37:43 -0300

    Add backend pratice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeaderHTML } from "./checkout/checkoutHeader.js";
+import "../data/backend-pratice.js";
 
 renderCheckoutHeaderHTML();
 renderOrderSummary();
